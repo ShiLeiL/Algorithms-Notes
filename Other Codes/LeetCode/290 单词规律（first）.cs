4c993d70af8c7a83832e85598856769c5c741993
ShiLeiL <33278967+ShiLeiL@users.noreply.github.com>
2019-11-23 18:36:37 +0800

    2019/11/23 Commit
    Routine~

---
+++ Other Codes/LeetCode/290 单词规律（first）.cs
@@ -0,0 +1,27 @@
+//这是第一次的提交记录
+//执行用时88ms，超过91.89%的C#提交记录=v=
+//思路是用字典储存pattern和str之间的映射
+//在遍历pattern的同时，对映射进行判断
+
+
+public class Solution 
+{
+    public bool WordPattern(string pattern, string str)
+    {
+        string[] strList = str.Split(" "); //切分str
+        if (pattern.Length != strList.Length) return false; //若pattern和str数量不对应，则直接说明不匹配
+        Dictionary<char, string> dic = new Dictionary<char, string>();
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (!dic.ContainsKey(pattern[i])) //若字典中无pattern的字母，表示应该要添加新的字母和字符的映射
+            {
+                if (!dic.ContainsValue(strList[i])) //且字典中也无str的字符，则添加映射
+                    dic.Add(pattern[i], strList[i]); 
+                else return false; //要么字典中有str的字符，则表示不匹配，直接返回
+            }
+            else if (dic[pattern[i]] != strList[i]) //若字典中有pattern字母，则表示有对应映射了，直接把pattern字母对应的字符进行对比，若不相等就说明不匹配
+                return false;
+        }
+        return true; //都匹配，返回匹配成功
+    }
+}
---
+++ Other Codes/LeetCode/290 单词规律（second）.cs
@@ -0,0 +1,28 @@
+//这是第二次的提交记录
+//执行用时84ms，超过97.3%的C#提交记录=v=
+//思路是同时对pattern和str进行遍历，判断当前字母和字符是否与前面的字母字符相等
+//若相等关系相同，则表示匹配，反之则不匹配
+
+
+public class Solution 
+{
+    public bool WordPattern(string pattern, string str)
+    {
+        string[] strList = str.Split(" ");
+        if (pattern.Length != strList.Length) return false; //若pattern和str数量不对应，则直接说明不匹配         
+        for(int i=1;i<pattern.Length;i++) //从第二个字母字符开始判断
+        {
+            for(int j=0;j<i;j++) //跟前面的字母字符进行对比
+            {
+                if (pattern[i] == pattern[j]) //若字母相等
+                {
+                    if (strList[i] != strList[j]) return false; //字符不相等，关系不同，不匹配
+                    else break; //字符相等，关系相同，直接退出当前字母的对比循环
+                }                       
+                else if(strList[i] == strList[j]) //若字母不相等，字母相等，关系不同，不匹配
+                    return false;
+            }
+        }
+        return true;
+    }
+}
